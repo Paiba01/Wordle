e82853b2f45a1892ba5f84f0ba1518316e9b6f9f
Jose Angel <doradojoseangel@gmail.com>
2022-05-07 21:16:42 +0200

    Empezando wordle con js

---
+++ ProyectoWordle/src/components/WordleGame.js
@@ -0,0 +1,165 @@
+import WORDS from "../assets/palabras.json";
+import "./WordleWord.js";
+
+class WordleGame extends HTMLElement {
+    constructor() {
+      super();
+      this.attachShadow({ mode: "open" });
+      this.startGame();
+    }
+  
+    static get styles() {
+      return /* css */`
+        :host {
+          --exact-color: #6aaa64;
+          --exist-color: #c9b458;
+          --used-color: #3a3a3c;
+          font-family: Montserrat, sans-serif;
+        }
+        .container {
+          display: flex;
+          flex-direction: column;
+          justify-content: space-between;
+          align-items: center;
+          height: 100vh;
+        }
+        h1 {
+          text-transform: uppercase;
+          border-bottom: 1px solid #555;
+          margin-bottom: 0;
+        }
+        h2 {
+          font-weight: lighter;
+          font-size: 1rem;
+          text-align: center;
+          margin: 0;
+        }
+        h2 a {
+          color: #f4b400;
+        }
+        h2 a:hover {
+          color: #a22;
+        }
+        .words {
+          display: flex;
+          flex-direction: column;
+          font-weight: bold;
+        }
+      `;
+    }
+  
+    startGame() {
+      const randomIndex = Math.floor(Math.random() * WORDS.length);
+      this.secretWord = WORDS[randomIndex].toLowerCase();
+      // console.log(this.secretWord);
+      this.ending = false;
+    }
+  
+    connectedCallback() {
+      this.render();
+      this.currentWord = this.shadowRoot.querySelector("wordle-word[current]");
+      this.keyboard = this.shadowRoot.querySelector("wordle-keyboard");
+      document.addEventListener("keyup", (ev) => this.pushLetter(ev.key));
+      document.addEventListener("keyboard", (ev) => this.pushLetter(ev.detail));
+      this.addEventListener("KEYBOARD_SET_LETTER", (ev) => this.keyboard.setLetter(ev.detail.letter, ev.detail.className));
+    }
+  
+    pushLetter(letter) {
+      if (this.ending) { return; }
+  
+      const key = letter.toLowerCase();
+      const isEnter = key === "enter";
+      const isBackSpace = key === "backspace";
+  
+      isEnter && this.checkRestrictions();
+      isBackSpace && this.currentWord.removeLetter();
+  
+      const isLetter = LETTERS.includes(key);
+      const isEmptyWord = this.currentWord.isEmpty();
+  
+      if (isLetter && isEmptyWord) {
+        this.currentWord.addLetter(key);
+      }
+    }
+  
+    checkRestrictions() {
+      const isEmpty = this.currentWord.isEmpty();
+      if (isEmpty) {
+        alert("La palabra debe tener 5 letras");
+        return;
+      }
+  
+      const word = this.currentWord.toString();
+      const existentWord = WORDS.includes(word);
+      if (!existentWord) {
+        alert("No existe esta palabra en el diccionario");
+        return;
+      }
+  
+      const solved = this.currentWord.resolve(this.secretWord);
+      if (!solved) {
+        this.nextWord();
+        return;
+      }
+  
+      this.win();
+    }
+  
+    nextWord() {
+      this.currentWord = this.shadowRoot.querySelector("wordle-word[current]");
+      const nextWord = this.currentWord.nextElementSibling;
+  
+      if (nextWord) {
+        nextWord.setAttribute("current", "");
+        this.currentWord.removeAttribute("current");
+        this.currentWord = nextWord;
+        return;
+      }
+  
+      this.lose();
+    }
+  
+    win() {
+      WIN_SOUND.play();
+      confetti();
+      this.startSummary(true);
+      this.ending = true;
+    }
+  
+    lose() {
+      LOSE_SOUND.play();
+      this.startSummary(false);
+      this.ending = true;
+    }
+  
+    startSummary(winner) {
+      const container = this.shadowRoot.querySelector(".container");
+      const summary = document.createElement("wordle-summary");
+      summary.setSecretWord(this.secretWord);
+      summary.setWinner(winner);
+      summary.setStats(this.shadowRoot.querySelectorAll("wordle-word"));
+      container.insertAdjacentElement("beforeend", summary);
+    }
+  
+    render() {
+      this.shadowRoot.innerHTML = /* html */`
+      <style>${WordleGame.styles}</style>
+      <div class="container">
+        <header>
+          <h1>Wordle</h1>
+          <h2>Hecho por <a>Joselete</a></h2>
+        </header>
+        <div class="words">
+          <wordle-word current></wordle-word>
+          <wordle-word></wordle-word>
+          <wordle-word></wordle-word>
+          <wordle-word></wordle-word>
+          <wordle-word></wordle-word>
+          <wordle-word></wordle-word>
+        </div>
+        <wordle-keyboard></wordle-keyboard>
+      </div>`;
+    }
+  }
+  
+  customElements.define("wordle-game", WordleGame);
