9d97215c7b199491af4af0f76ed2c7795c3023ac
Jose Angel <doradojoseangel@gmail.com>
2022-05-08 23:32:39 +0200

    Registro en BD

--- ProyectoWordle/src/assets/php/registro_usuario_be.php
+++ ProyectoWordle/src/assets/php/registro_usuario_be.php
@@ -1,16 +1,49 @@
 <?php
+    $servername = "localhost";
+    $username = "id18899575_p92dogoj";
+    $password = "acd//a-|^0pW|@3A";
+    $database = "id18899575_wordlebd";
 
-    include 'conexion_be.php'
+    try {  
+        //Gets the insertion data
+        $username1 = $_REQUEST["username"];
+        $password1 = $_REQUEST["password"];
+        $nombre = $_REQUEST["nombre"];
+        $apellidos = $_REQUEST["apellidos"];
+        $email = $_REQUEST["email"];
 
-    $username = $_POST['username'];
-    $password = $_POST['password'];
-    $nombre = $_POST['nombre'];
-    $apellidos = $_POST['apellidos'];
-    $email = $_POST['email'];
+        if ($username1 == "" || $password1 == "" || $nombre == "" || $apellidos == "" || $email == "") {
+            http_response_code(400);
+            echo "Bad Request";
+            return;
+        }
 
-    $query = "INSERT INTO usuarios(username, password, nombre, apellidos, email) 
-              VALUES('$username', '$password', '$nombre', '$apellidos', '$email')";
+        //tries to connect to the databse
+        $conn = new PDO("mysql:host=$servername;dbname=$database", $username, $password);
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-    $ejecutar = mysqli_query($conexion, $query);
+        //Executes an insert sql query
+        $table = "usuarios";
+        $sql = "INSERT INTO usuarios (username, password, nombre, apellidos, email) VALUES ('$username1', '$password1', '$nombre', '$apellidos', '$email')";
+        $result = $conn->exec($sql);
+        if (!$result) {
+            http_response_code(500);
+            print "ERROR: " . $conn->errorMsg() . "\r\n";
+        }
+        else {
+            http_response_code(200);
+            print $name . " was succesfully added to the database \r\n";
+        }
 
+    }
+    catch (PDOException $e) {
+        if ($e->errorInfo[1] == 1062) {
+            http_response_code(205);
+            echo "usuarios already added";
+        }
+        else {
+            http_response_code(500);
+            echo "Connection failed: " . $e->getMessage();
+        }
+    }
 ?>
